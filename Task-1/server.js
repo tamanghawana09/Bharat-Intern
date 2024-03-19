3bd4cb741ea6ee7a0f94c18694033871c0b0e051
ergophile-geek <hawanatamang@gmail.com>
2024-03-19 23:31:19 +0545

    add: Add dependencies

---
+++ Task-1/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+app.listen(3000)
